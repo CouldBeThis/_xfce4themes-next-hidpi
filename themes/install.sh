1b3e925152c4ed613f5f8903590c15316c5875c3
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-04-21 19:09:26 -0400

    update readme and install script to include slimline 2022 HiDPI

--- themes/install.sh
+++ themes/install.sh
@@ -4,6 +4,8 @@ echo "Installing themes for all users (root)"
 
 cp -r slimline-2022 "/usr/share/themes/2022 Slimline"
 
+cp -r slimline-2022-HiDPI "/usr/share/themes/2022 Slimline (HiDPI)"
+
 cp -r Trench-2022 "/usr/share/themes/2022 Trench"
 
 xfwm4-settings & 
