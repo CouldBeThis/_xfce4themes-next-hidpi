057091478eef3ada5f0559d3ac7efd964e0c9883
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-05-22 19:07:15 -0400

    rename files to inactive

---
+++ next-2022-HDPI/xfwm4/generate-secondary-images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## this is a skeleton I am making as I work through the steps manually; to be actually written later
+
+## create temporary working directory "inactive"
+
+## copy all files *-active.xpm" into diretcory "inactive"
+
+##
+
+
+
+
+
+
+
+
+
+
+
